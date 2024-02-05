0eb791df6465fc525beb9ca8b415957d3b2f2d85
Marcus Lo <marcusklo1010@gmail.com>
2024-02-05 11:50:44 -0700

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names, gift) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}`;
+      if (gift) {
+        message += `, for the wonderful ${gift} gift!`;
+      } else {
+        message += '!';
+      }
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
